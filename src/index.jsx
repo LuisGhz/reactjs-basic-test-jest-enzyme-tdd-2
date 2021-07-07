ea39a3d2e52f8bc4817e61da48f2dbf3ddfff524
Luis Gutiérrez <luisghtz@outlook.com>
2021-07-07 12:22:32 -0500

    Change default content by course content

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as reducers from './reducers';
+import * as serviceWorker from './serviceWorker';
+
+const store = createStore(combineReducers({
+  ...reducers,
+}));
+
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
@@ -15,3 +24,9 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
+
