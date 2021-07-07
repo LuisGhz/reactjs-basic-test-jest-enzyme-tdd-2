0b3d14f85a2cf851dfd60fbd9de1040d6a6f575f
Luis Gutiérrez <luisghtz@outlook.com>
2021-07-07 12:58:08 -0500

    Move Form component to its own file Import Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import { connect } from "react-redux";
 import { agregar, eliminar } from "./reducers/finanzas";
+import Form from "components/Form";
 import "./App.css";
 
 function Titulo() {
   return <h2 className="title">Finanzly</h2>;
-}
-
-function Form({ agregarFinanza }) {
-  const [cant, setCant] = useState("");
-  const [desc, setDesc] = useState("");
-
-  const handleSubmit = e => {
-    e.preventDefault();
-    agregarFinanza({ desc, cant: Number(cant) });
-    setDesc("");
-    setCant("");
-  };
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <div className="column is-half">
-        <div className="field">
-          <p className="control has-icons-left">
-            <input
-              className="input"
-              value={desc}
-              placeholder="Descripción"
-              onChange={e => setDesc(e.target.value)}
-              type="text"
-            />
-            <span className="icon is-small is-left">
-              <i className="fas fa-align-justify" />
-            </span>
-          </p>
-        </div>
-        <div className="field">
-          <p className="control has-icons-left">
-            <input
-              className="input"
-              value={cant}
-              placeholder="Cantidad"
-              onChange={e => setCant(e.target.value)}
-              type="number"
-            />
-            <span className="icon is-small is-left">
-              <i className="fas fa-money-bill-alt" />
-            </span>
-          </p>
-        </div>
-        <button className="button is-primary" type="submit" value="Enviar">
-          Enviar
-        </button>
-      </div>
-    </form>
-  );
 }
 
 function Dashboard({ valor }) {
