1168d14bb462bdaf19085f9f98cdc4ade74d7921
Luis Gutiérrez <luisghtz@outlook.com>
2021-07-07 16:58:19 -0500

    Create test for App

---
+++ src/App.test.js
@@ -0,0 +1,11 @@
+import { mount, configure } from "enzyme";
+import Adapter from 'enzyme-adapter-react-17-updated';
+import App from 'App';
+
+configure({
+  adapter: new Adapter()
+})
+
+describe('App', () => {
+
+});
