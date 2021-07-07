abf855208de0cd2cd8e6bb523faa2d69ce154af2
Luis Gutiérrez <luisghtz@outlook.com>
2021-07-07 13:22:16 -0500

    Create Dashboard component

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,13 @@
+
+const Dashboard = ({ valor }) => {
+  return (
+    <div className="column is-half">
+      <div className="box">
+        <p>Total</p>
+        <strong>{valor}</strong>
+      </div>
+    </div>
+  );
+}
+
+export default Dashboard;
