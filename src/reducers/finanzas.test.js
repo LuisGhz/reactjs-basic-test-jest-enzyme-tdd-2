b371f33a50f2b211eceb9c82c6b21d907bd9b582
Luis Gutiérrez <luisghtz@outlook.com>
2021-07-07 12:47:12 -0500

    Add test to Add action

--- src/reducers/finanzas.test.js
+++ src/reducers/finanzas.test.js
@@ -4,6 +4,16 @@ describe('Finanzas Duck', () => {
 
   describe('Action creators', () => {
     
+    it('Add', () => {
+      const result = agregar(1);
+
+      expect(result).toEqual({
+        type: 'AGREGAR',
+        payload: 1
+      });
+      
+    });
+
   });
 
 })
